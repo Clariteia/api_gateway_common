c5a77667ed83995b8d2438e6db5c0c06746b07db
Restyled.io <commits@restyled.io>
2021-05-14 15:20:04 +0000

    Restyled by autopep8

--- tests/test_api_gateway/test_common/test_client/test_abc.py
+++ tests/test_api_gateway/test_common/test_client/test_abc.py
@@ -10,19 +10,18 @@ class TestClient(IsolatedAsyncioTestCase):
         request = await client.get(url="http://httpbin.org/get")
         self.assertEqual(request.status, 200)
 
-        #Test parameters
+        # Test parameters
         params = {"test": 1}
         request = await client.get(url="http://httpbin.org/get", params=params)
         self.assertEqual(request.url.query_string, "test=1")
         self.assertEqual(request.status, 200)
-
 
     async def test_post(self):
         client = ClientHttp()
         request = await client.post(url="http://httpbin.org/post")
         self.assertEqual(request.status, 200)
 
-        #Test parameters
+        # Test parameters
         params = {"test": 1}
         request = await client.post(url="http://httpbin.org/post", params=params)
         self.assertEqual(request.url.query_string, "test=1")
@@ -33,13 +32,12 @@ class TestClient(IsolatedAsyncioTestCase):
         request = await client.post(url="http://httpbin.org/post", params=params, data=data)
         self.assertEqual(request.status, 200)
 
-
     async def test_put(self):
         client = ClientHttp()
         request = await client.put(url="http://httpbin.org/put")
         self.assertEqual(request.status, 200)
 
-        #Test parameters
+        # Test parameters
         params = {"test": 1}
         request = await client.put(url="http://httpbin.org/put", params=params)
         self.assertEqual(request.url.query_string, "test=1")
@@ -55,7 +53,7 @@ class TestClient(IsolatedAsyncioTestCase):
         request = await client.patch(url="http://httpbin.org/patch")
         self.assertEqual(request.status, 200)
 
-        #Test parameters
+        # Test parameters
         params = {"test": 1}
         request = await client.patch(url="http://httpbin.org/patch", params=params)
         self.assertEqual(request.url.query_string, "test=1")
@@ -71,7 +69,7 @@ class TestClient(IsolatedAsyncioTestCase):
         request = await client.delete(url="http://httpbin.org/delete")
         self.assertEqual(request.status, 200)
 
-        #Test parameters
+        # Test parameters
         params = {"test": 1}
         request = await client.delete(url="http://httpbin.org/delete", params=params)
         self.assertEqual(request.url.query_string, "test=1")
