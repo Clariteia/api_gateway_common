8884f5897b1635bf10e7c9dfbc5bbb733000d763
Restyled.io <commits@restyled.io>
2021-05-17 17:23:33 +0000

    Restyled by isort

--- docs/conf.py
+++ docs/conf.py
@@ -12,14 +12,17 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
+import os
+import sys
+
 # If extensions (or modules to document with autodoc) are in another
 # directory, add these directories to sys.path here. If the directory is
 # relative to the documentation root, use os.path.abspath to make it
 # absolute, like shown here.
 #
-from minos.api_gateway import common
-import os
-import sys
+from minos.api_gateway import (
+    common,
+)
 
 sys.path.insert(0, os.path.abspath(".."))
 
